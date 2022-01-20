6f3c5b0aff0b30cccad770bb0961acdc9fe56290
Bhanu Prasad M <mbhanuprsd@gmail.com>
2022-01-20 14:32:38 +0530

    Created empty pages and base layout file

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+const App = () => (
+    <Router>
+        <Switch>
+
+        </Switch>
+    </Router>
+);
+
+export default App;
