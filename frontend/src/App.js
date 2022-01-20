e0ab475e5062ae4328576ea4eeb7a6a4905005c0
Bhanu Prasad M <mbhanuprsd@gmail.com>
2022-01-20 14:55:31 +0530

    Updated components with text and Added home route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./containers/Home";
+import Layout from "./hocs/Layout";
 
 const App = () => (
     <Router>
-        <Switch>
-
-        </Switch>
+        <Layout>
+            <Switch>
+                <Route exact path='/' component={Home} />
+            </Switch>
+        </Layout>
     </Router>
 );
 
