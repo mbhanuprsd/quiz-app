949cb16205a585b778ab9b79c952e2cd70f4466c
Bhanu Prasad M <mbhanuprsd@gmail.com>
2022-01-20 15:05:37 +0530

    Added routes for all pages

---
+++ backend/README.md
@@ -0,0 +1,1 @@
+https://djoser.readthedocs.io/en/latest/base_endpoints.html#
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Activate from "./containers/Activate";
 import Home from "./containers/Home";
+import Login from "./containers/Login";
+import ResetPassword from "./containers/ResetPassword";
+import ResetPasswordConfirm from "./containers/ResetPasswordConfirm";
+import SignUp from "./containers/SignUp";
 import Layout from "./hocs/Layout";
 
 const App = () => (
@@ -8,6 +13,11 @@ const App = () => (
         <Layout>
             <Switch>
                 <Route exact path='/' component={Home} />
+                <Route exact path='/login' component={Login} />
+                <Route exact path='/signup' component={SignUp} />
+                <Route exact path='/reset-password' component={ResetPassword} />
+                <Route exact path='/password/reset/confirm/:uid/:token' component={ResetPasswordConfirm} />
+                <Route exact path='/activate/:uid/:token' component={Activate} />
             </Switch>
         </Layout>
     </Router>
